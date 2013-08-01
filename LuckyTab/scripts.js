c0b5d1f2ede3c02938c1d59abcba19c489baea3b
Asaf Yonay <asafy3@gmail.com>
2013-08-01 23:58:44 +0300

    Finalized hookups.

--- LuckyTab/scripts.js
+++ LuckyTab/scripts.js
@@ -1,9 +1,26 @@
 function getTopSites(callbackfunc) {
     chrome.topSites.get(function(url_list) {
-        for (var i=0;i<10;i++) {callbackfunc(url_list[i]);}
+        for (var i=0;i<url_list.length;i++) {callbackfunc(url_list[i]);}
     });
 }
 getTopSites(function(url){
 	var thumbnailUrl = 'chrome://favicon/size/16@1x/' + url.url;
 	$('#mostVisited ul').append("<li><a style='background:url("+thumbnailUrl+") no-repeat 2px 2px' href='"+url.url+"'>"+url.title+"</a></li>");
 });
+//DOM ready
+var xhr = new XMLHttpRequest();
+		xhr.open("GET", "http://82.196.3.219/getKey", true);
+		xhr.onreadystatechange = function() {			
+  		if (xhr.readyState == 4) { 
+  			console.log(xhr.responseText);   		
+    		var responseObj = JSON.parse(xhr.responseText);
+    		$("body").css("background-image","url("+responseObj.image+")");
+    		$("#pageTitle").html(responseObj.title).attr("href",responseObj.url).css("display","block");	
+			$("#pageDesc").html(responseObj.desc).css("display","block");
+			$("#readPage").attr("href",responseObj.url).css("display","inline");
+			$("#pageSource").html("From: "+responseObj.source).css("display","block");
+  		}
+	}
+xhr.send();
+
+
--- luckyTabCurate/scripts.js
+++ luckyTabCurate/scripts.js
@@ -1,11 +1,53 @@
 chrome.tabs.getSelected(null, function(tab) {
     populate(tab.url);
 });
-function populate(url) {
-  // do stuff here
+function populate(url) { 
   tablink = url;
-  $('#url').val(tablink);
+  $('#pageURL').val(tablink);
 }
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
+
+function concatoneString(){	
+	var sendUrl = 'http://82.196.3.219/setKey?value="title":"{0}","desc":"{1}","url":"{2}","source":"{3}","image":"{4}","category":"{5}","timeless":"{6}"';
+	var title = $("#pageTitle").val();
+	var desc = $("#pageDesc").val();
+	var url = $("#pageURL").val();
+	var source = $("#pageSource").val();
+	var image = $("#pageImage").val();
+	var e = document.getElementById("pageCategory");
+	var category = e.options[e.selectedIndex].text;	
+	var isTimeless = document.getElementById("isTimeless").checked == true ? "yes": "no";
+	sendUrl = sendUrl.format(title,desc,url,source,image,category,isTimeless);
+	console.log(sendUrl);	
+	
+	
+	var xhr = new XMLHttpRequest();
+		xhr.open("GET", sendUrl, true);
+		xhr.onreadystatechange = function() {			
+  		if (xhr.readyState == 4) { 
+  			console.log(xhr.responseText);
+  		}
+	}
+	xhr.send();
+}
+
+window.addEventListener("DOMContentLoaded",function(){
+	$("#sendForm").click(function(e){
+		e.preventDefault();
+		concatoneString();
+	})
+})
+
 
 
 
