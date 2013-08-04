5dfc774e853878a0196002d1883a6f3963efc903
Asaf Yonay <asafy3@gmail.com>
2013-08-04 20:18:00 +0300

    Moved set calls to new method (sort by category)

--- PoshfeedCurate/inject.js
+++ PoshfeedCurate/inject.js
@@ -41,12 +41,12 @@ function populate(url,title,descrip) {
 }
 
 function sendKey(){	
-	var sendUrl = 'http://82.196.3.219/setKey?value="title":"{0}","desc":"{1}","url":"{2}","source":"{3}","image":"{4}","category":"{5}","timeless":"{6}"';
+	var sendUrl = 'http://82.196.3.219/setCategoryKey?value="title":"{0}","desc":"{1}","url":"{2}","source":"{3}","image":"{4}","category":"{5}"';
 	var title = encodeURIComponent($("#pageTitle").val());
 	var desc = encodeURIComponent($("#pageDesc").val());
 	var url = encodeURIComponent($("#pageURL").val());
 	var source = encodeURIComponent($("#pageSource").val());
-	var image = encodeURIComponent($("#pageImage").val());
+	var image = encodeURIComponent($("#pageImage").val());	
 	var e = document.getElementById("pageCategory");
 	var category = encodeURIComponent(e.options[e.selectedIndex].text);;	
 	//var isTimeless = document.getElementById("isTimeless").checked == true ? "yes": "no";
