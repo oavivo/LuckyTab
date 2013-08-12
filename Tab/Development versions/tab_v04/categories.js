b34f8a6edfd9303ca403511d907d6c5f38560807
Ohad Aviv <ohad@wibiya.com>
2013-08-12 13:53:15 +0300

    new tab files

---
+++ Tab/Development versions/tab_v04/categories.js
@@ -0,0 +1,20 @@
+
+var PFcategoryList = [
+	{id:"geeky",display:"Geeky"},
+	{id:"intelectual",display:"Intelectual"},
+	{id:"music",display:"Musicphile"},
+	{id:"food",display:"Foodie"},
+	{id:"art",display:"Artsy"},
+	{id:"cool",display:"Cool"}
+];
+
+var categoriesObj = {
+	"geeky":{id:"geeky",display:"Geeky"},
+	"intelectual": {id:"intelectual",display:"Intelectual"},
+	"music": {id:"music",display:"Musicphile"},
+	"food": {id:"food",display:"Foodie"},
+	"art": {id:"art",display:"Artsy"},
+	"cool": {id:"cool",display:"Cool"}
+}
+
+
---
+++ Tab/Development versions/tab_v04/front.js
@@ -0,0 +1,70 @@
+
+function getTopSites(callbackfunc) {
+    chrome.topSites.get(function(url_list) {
+        for (var i=0;i<url_list.length;i++) {callbackfunc(url_list[i]);}
+    });
+}
+getTopSites(function(url){
+	var thumbnailUrl = 'chrome://favicon/' + url.url;
+	$('#mostVisited ul').append("<li><a style='background:url("+thumbnailUrl+") no-repeat 2px 2px' href='"+url.url+"'>"+url.title+"</a></li>");
+});
+
+function fireClickEvent(redirectURL){
+	var eventXHR = new XMLHttpRequest();
+		eventXHR.open("GET", "http://82.196.3.219/addClickStat", true);
+		eventXHR.onreadystatechange = function() {
+			if (eventXHR.readyState == 4) {
+				console.log(xhr.responseText);
+			}
+		}	
+	eventXHR.send();
+	setTimeout(function(){window.location.href = redirectURL},200);
+}
+
+function getContent(cats){
+	var xhr = new XMLHttpRequest();
+			xhr.open("GET", "http://poshfeed.com/getCategoryKey?categories="+cats, true);
+			xhr.onreadystatechange = function() {			
+	  		if (xhr.readyState == 4) {  			 		
+	    		var responseObj = JSON.parse(xhr.responseText);    		
+	    		window.returnedJson = responseObj;
+	    		$("body").css("background-image","url("+responseObj.image+")");
+	    		$("#pageTitle").html(responseObj.title).attr("href",responseObj.url).css("display","block").click(function(e){
+	    			e.preventDefault();
+	    			fireClickEvent(responseObj.url);    			    			
+	    		});	
+				if (responseObj.desc) {$("#pageDesc").html(responseObj.desc).attr("href",responseObj.url).css("display","block").click(function(e){
+	    			e.preventDefault();
+	    			fireClickEvent(responseObj.url);    			    			
+	    		});	
+	    		} //show only if description available
+				$("#readPage").attr("href",responseObj.url).css("display","inline").click(function(e){
+	    			e.preventDefault();
+	    			fireClickEvent(responseObj.url);    			    			
+	    		});
+				//var catDisplayName = $.inArray('food', PFcategoryList) > -1; // left this in the middle
+				//console.log(catDisplayName);
+				//$("#pageSource").html("From: "+responseObj.source +", In: "+responseObj.category).css("display","block");
+	  		}
+		}
+	xhr.send();
+}
+chrome.storage.sync.get("categories", function(data){	
+	data = $(data.categories).toArray();
+	var displayNameArray = [];
+	for(var i=0;i < data.length ; i++){
+		displayNameArray.push(categoriesObj[data[i]].display)
+	}
+	$('#currentCats').text(displayNameArray.join(', '));
+	if(data.length == 0){
+		data = ["food","artDesign","gadgets","techNews","artCraft","autos","fashion","music","science","humor","lifestyle"];
+		chrome.storage.sync.set({'categories': data});
+	}
+	data = data.toString();	
+	
+	$('#optionsLink').attr('href',chrome.extension.getURL("options.html"));
+	getContent(data);
+})
+
+
+
