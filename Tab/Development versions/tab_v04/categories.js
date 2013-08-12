9959e0791ebda96300e3fef760f172cdce9de401
Ohad Aviv <ohad@wibiya.com>
2013-08-12 15:36:25 +0300

    styling the new tab

--- Tab/Development versions/tab_v04/categories.js
+++ Tab/Development versions/tab_v04/categories.js
@@ -1,20 +1,11 @@
-
-var PFcategoryList = [
-	{id:"geeky",display:"Geeky"},
-	{id:"intelectual",display:"Intelectual"},
-	{id:"music",display:"Musicphile"},
-	{id:"food",display:"Foodie"},
-	{id:"art",display:"Artsy"},
-	{id:"cool",display:"Cool"}
-];
-
 var categoriesObj = {
 	"geeky":{id:"geeky",display:"Geeky"},
 	"intelectual": {id:"intelectual",display:"Intelectual"},
 	"music": {id:"music",display:"Musicphile"},
 	"food": {id:"food",display:"Foodie"},
 	"art": {id:"art",display:"Artsy"},
-	"cool": {id:"cool",display:"Cool"}
+	"cool": {id:"cool",display:"Cool"},
+	"cool": {id:"fashion",display:"Fashionable"}
 }
 
 
--- Tab/Development versions/tab_v04/front.js
+++ Tab/Development versions/tab_v04/front.js
@@ -44,7 +44,7 @@ function getContent(cats){
 	    		});
 				//var catDisplayName = $.inArray('food', PFcategoryList) > -1; // left this in the middle
 				//console.log(catDisplayName);
-				//$("#pageSource").html("From: "+responseObj.source +", In: "+responseObj.category).css("display","block");
+				$("#pageSource").html("From: "+responseObj.source).css("display","");
 	  		}
 		}
 	xhr.send();
