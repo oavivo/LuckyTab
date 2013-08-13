51ef59826359fda613d3def90539dbfd05361066
Ohad Aviv <ohad@wibiya.com>
2013-08-13 18:11:38 +0300

    added analytics

--- Tab/Current version/front.js
+++ Tab/Current version/front.js
@@ -72,4 +72,13 @@ chrome.storage.sync.get("categories", function(data){
 });
 
 
-
+// google analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-43001046-1']);
+_gaq.push(['_trackPageview']);
+ 
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
