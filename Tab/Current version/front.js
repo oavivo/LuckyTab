44bd8dd497e4778e4af1d0dcc7096843a2b737fd
Ohad Aviv <ohad@wibiya.com>
2013-08-13 10:38:16 +0300

    new version deployed

--- Tab/Current version/front.js
+++ Tab/Current version/front.js
@@ -1,13 +1,75 @@
-function getTopSites(e){chrome.topSites.get(function(t){for(var n=0;n<t.length;n++){e(t[n])}})}function fireClickEvent(e){var t=new XMLHttpRequest;t.open("GET","http://poshfeed.com/addClickStat?value="+encodeURIComponent(returnedJson),true);t.onreadystatechange=function(){if(t.readyState==4){console.log(xhr.responseText)}};t.send();setTimeout(function(){window.location.href=e},200)}function getContent(){var e=new XMLHttpRequest;e.open("GET","http://poshfeed.com/getKey",true);e.onreadystatechange=function(){if(e.readyState==4){window.returnedJson=e.responseText.substring(1,e.responseText.length-1);var t=JSON.parse(e.responseText);$("body").css("background-image","url("+t.image+")");$("#pageTitle").html(t.title).attr("href",t.url).css("display","block").click(function(e){e.preventDefault();fireClickEvent(t.url)});if(t.desc){$("#pageDesc").html(t.desc).attr("href",t.url).css("display","block").click(function(e){e.preventDefault();fireClickEvent(t.url)})}$("#readPage").attr("href",t.url).css("display","inline").click(function(e){e.preventDefault();fireClickEvent(t.url)});$("#pageSource").html("From: "+t.source).css("display","block")}};e.send()}getTopSites(function(e){var t="chrome://favicon/"+e.url;$("#mostVisited ul").append("<li><a style='background:url("+t+") no-repeat 2px 2px' href='"+e.url+"'>"+e.title+"</a></li>")});getContent()
+
+function getTopSites(callbackfunc) {
+    chrome.topSites.get(function(url_list) {
+        for (var i=0;i<url_list.length;i++) {callbackfunc(url_list[i]);}
+    });
+}
+getTopSites(function(url){
+	var thumbnailUrl = 'chrome://favicon/' + url.url;
+	$('#mostVisited ul').append("<li><a style='background:url("+thumbnailUrl+") no-repeat 2px 2px' href='"+url.url+"'>"+url.title+"</a></li>");
+});
+
+function fireClickEvent(redirectURL){
+	var eventXHR = new XMLHttpRequest();
+		eventXHR.open("GET", "http://82.196.3.219/addClickStat", true);
+		eventXHR.onreadystatechange = function() {
+			if (eventXHR.readyState == 4) {
+				console.log(xhr.responseText);
+			}
+		}	
+	eventXHR.send();
+	setTimeout(function(){window.location.href = redirectURL},200);
+}
+
+function getContent(cats){
+	var xhr = new XMLHttpRequest();
+			xhr.open("GET", "http://poshfeed.com/getCategoryKey?categories="+cats, true);
+			xhr.onreadystatechange = function() {			
+	  		if (xhr.readyState == 4) {  			 		
+	    		var responseObj = JSON.parse(xhr.responseText);    		
+	    		window.returnedJson = responseObj;
+	    		$("body").css("background-image","url("+responseObj.image+")");
+	    		$("#pageTitle").html(responseObj.title).attr("href",responseObj.url).css("display","block").click(function(e){
+	    			e.preventDefault();
+	    			fireClickEvent(responseObj.url);    			    			
+	    		});	
+				if (responseObj.desc) {$("#pageDesc").html(responseObj.desc).attr("href",responseObj.url).css("display","block").click(function(e){
+	    			e.preventDefault();
+	    			fireClickEvent(responseObj.url);    			    			
+	    		});	
+	    		} //show only if description available
+				$("#readPage").attr("href",responseObj.url).css("display","inline").click(function(e){
+	    			e.preventDefault();
+	    			fireClickEvent(responseObj.url);    			    			
+	    		});
+				//var catDisplayName = $.inArray('food', PFcategoryList) > -1; // left this in the middle
+				//console.log(catDisplayName);
+				$("#pageSource").html("From: "+responseObj.source).css("display","");
+	  		}
+		}
+	xhr.send();
+}
+chrome.storage.sync.get("categories", function(data){	
+	data = $(data.categories).toArray();
+	var displayNameArray = [];
+	for(var i=0;i < data.length ; i++){
+        var catName = data[i];
+		displayNameArray.push(categoriesObj[catName].display)
+	}
+	$('#currentCats').text(displayNameArray.join(', '));
+	if(data.length == 0){
+		var allCats = [];
+		for(cat in categoriesObj){
+			allCats.push(cat);			
+		}
+		data = allCats;
+		chrome.storage.sync.set({'categories': allCats});
+	}
+	data = data.toString();	
+	
+	$('#optionsLink').attr('href',chrome.extension.getURL("options.html"));
+	getContent(data);
+});
 
 
-// google analytics
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-43001046-1']);
-_gaq.push(['_trackPageview']);
 
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
