b98a8885f0b1e8290e498889b20af972e57e6a9d
Asaf Yonay <asafy3@gmail.com>
2013-08-15 22:55:55 +0300

    Added inline install when applicable (fallback to normal url if not) Added proper closing to our contact form

--- Website/assets/js/main.js
+++ Website/assets/js/main.js
@@ -42,12 +42,30 @@ function validateContactForm(){
 		var e = document.getElementById("Subject");
 		var contactSubject = encodeURIComponent(e.options[e.selectedIndex].value);
 		emailBody = emailBody.format(encodeURIComponent($("#contactFormBody #Name").val()),contactSubject,encodeURIComponent($("#contactFormBody #Email").val()),encodeURIComponent($("#contactFormBody #Message").val()));
-		var requestURL = 'http://poshfeed.com/sendContactForm?text='+emailBody;
-		$.get(requestURL, function(data) {});
-		$('#myModal').modal('hide');
+		var requestURL = 'http://poshfeed.com/sendContactForm?text='+emailBody;		
+		$.get(requestURL, function(data) {
+			setTimeout(function(){
+				$('#contactForm').modal('hide');				
+			},3000);
+			$(".modal-footer").remove();
+			$("#contactFormBody").html("<h4>Message sent</h4><div class='contactLabel'>Thank you!</div>");
+			$('#contactForm').on('hidden.bs.modal', function () {$('#contactForm').html(formHTML)})
+		});
+		
 	}
 	
 }
 
 
 $("#sendFormButton").click(validateContactForm);
+window.formHTML = $("#contactForm").html();
+$(".appStoreLink").click(function(e){	
+	// Only apply inline install functionality to Chrome 15 and above
+	var chromeVersion = window.navigator.userAgent.match(/Chrome\/([0-9]*)/);	
+	if(chromeVersion != null && parseInt(chromeVersion[1]) > 15){
+		e.preventDefault();
+		chrome.webstore.install("",function(){},function(){
+			document.location.href = "https://chrome.google.com/webstore/detail/poshfeed/aimbgnciobahnpjegjhidihgoaipdabm";
+		});
+	}
+});
