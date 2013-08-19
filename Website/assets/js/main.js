dc6266f3989cc40854184909ff043bce7d07b5e7
Asaf Yonay <asafy3@gmail.com>
2013-08-19 16:18:08 +0300

    Added custom events to inline install

--- Website/assets/js/main.js
+++ Website/assets/js/main.js
@@ -37,7 +37,8 @@ function validateContactForm(){
 			}
 		}
 	})
-	if(validForm){		
+	if(validForm){
+	
 		var emailBody = "Name:%20{0}<br/><br/>Subject:%20{1}<br/><br/>Email:%20{2}<br/><br/>Message:%20{3}";
 		var e = document.getElementById("Subject");
 		var contactSubject = encodeURIComponent(e.options[e.selectedIndex].value);
@@ -47,7 +48,8 @@ function validateContactForm(){
 			setTimeout(function(){
 				$('#contactForm').modal('hide');				
 			},3000);
-			$(".modal-footer").remove();
+			$(".modal-footer").remove();			
+			_gaq.push(['_trackEvent','contactForm','formSent','true']);		
 			$("#contactFormBody").html("<h4>Message sent</h4><div class='contactLabel'>Thank you!</div>");
 			$('#contactForm').on('hidden.bs.modal', function () {$('#contactForm').html(formHTML)})
 		});
@@ -60,15 +62,20 @@ function validateContactForm(){
 $("#sendFormButton").click(validateContactForm);
 window.formHTML = $("#contactForm").html();
 $(".appStoreLink").click(function(e){	
-	e.preventDefault();		
+	e.preventDefault();	
+	_gaq.push(['_trackEvent','downloadLinkClicked','inlineInstall','initialClick']);
 	// Only apply inline install functionality to Chrome 15 and above
 	var chromeVersion = window.navigator.userAgent.match(/Chrome\/([0-9]*)/);	
 	if(chromeVersion != null && parseInt(chromeVersion[1]) > 15){		
-		chrome.webstore.install("",function(){},function(err){
-			//On error:			
-			// If the user canceled the installation, keep them on the page, else redirect to our chromestore page
+		chrome.webstore.install("",function(){			
+			_gaq.push(['_trackEvent','downloadLinkClicked','inlineInstall','success'])		
+		},function(err){
+			//On error:	If the user canceled the installation, keep them on the page, else redirect to our chromestore page						
 			if(err != "User cancelled install"){
+				_gaq.push(['_trackEvent','downloadLinkClicked','inlineInstall','fail_'+err]);
 				document.location.href = "https://chrome.google.com/webstore/detail/poshfeed/aimbgnciobahnpjegjhidihgoaipdabm";
+			}else{
+				_gaq.push(['_trackEvent','downloadLinkClicked','inlineInstall','fail_userAborted'])
 			}
 		});
 	}else{
