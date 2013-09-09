0bf7c1a93765ed522271c1fc2cde8e681ad2d470
Ohad Aviv <ohad@wibiya.com>
2013-09-09 09:48:47 +0300

    fixed typo. added copyright notice.

--- Tab/categories.js
+++ Tab/categories.js
@@ -1,6 +1,6 @@
 var categoriesObj = {
 	"geeky":{id:"geeky",display:"Geeky"},
-	"intelectual": {id:"intelectual",display:"Intelectual"},
+	"intelectual": {id:"intelectual",display:"Intellectual"},
 	"music": {id:"music",display:"Musicphile"},
 	"food": {id:"food",display:"Foodie"},
 	"art": {id:"art",display:"Artsy"},
