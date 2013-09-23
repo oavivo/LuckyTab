28230921330fa3fd4b2c13ea89cb2dee21dc1f28
Ohad Aviv <ohad@wibiya.com>
2013-09-23 15:14:05 +0300

    ORDER!

--- Tab/front.js
+++ Tab/front.js
@@ -1,136 +1,105 @@
-if (!String.prototype.format) {
-    String.prototype.format = function() {
-        var args = arguments;
-        return this.replace(/{(\d+)}/g, function(match, number) {
-            return typeof args[number] != 'undefined'
-                ? args[number]
-                : match
-                ;
-        });
-    };
-}
-function getTopSites(callbackfunc) {
-    chrome.topSites.get(function(url_list) {
-        for (var i=0;i<url_list.length;i++) {callbackfunc(url_list[i]);}
-    });
-}
-getTopSites(function(url){
-	var thumbnailUrl = 'chrome://favicon/' + url.url;
-	$('#mostVisited ul').append("<li><a style='background-image:url("+thumbnailUrl+")' href='"+url.url+"'>"+url.title+"</a></li>");
-});
+//GLOBAL VARS
+var theCategories;
+var theContent;
 
-function fireClickEvent(redirectURL){
-	var eventXHR = new XMLHttpRequest();
-		eventXHR.open("GET", "http://82.196.3.219/addClickStat", true);
-		eventXHR.onreadystatechange = function() {
-			if (eventXHR.readyState == 4) {
-				console.log(xhr.responseText);
-			}
-		}	
-	eventXHR.send();
-	setTimeout(function(){window.location.href = redirectURL},200);
+
+//SUPPORT FUNCTIONS
+	//string formatting
+	if (!String.prototype.format) {String.prototype.format = function() {var args = arguments;return this.replace(/{(\d+)}/g, function(match, number) {return typeof args[number] != 'undefined'? args[number]: match;});};}
+////////////////////////////////////////////////////////////////////////////////////////////////////	
+
+
+//      SHARE FUNCTION      /////////////////////////////////////////////////////////////
+function getShareLink(e){
+	_trackEvent('click', 'FBShareButton', 'Facebook share button click');
+	value = theContent;
+	var sendUrl = 'http://poshfeed.com/shareURL?value="title":"{0}","desc":"{1}","url":"{2}","source":"{3}","image":"{4}","category":"{5}"';
+    var title = encodeURIComponent(value.title.replace(/\"/g,'%22'));
+    var desc = encodeURIComponent(value.desc.replace(/\"/g,'\%22'));
+    var url = encodeURIComponent(value.url.replace(/\"/g,'%22'));
+    var source = encodeURIComponent(value.source.replace(/\"/g,'%22'));
+    var image = encodeURIComponent(value.image.replace(/\"/g,'%22'));
+    var category = encodeURIComponent(value.category.replace(/\"/g,'%22'))
+
+    sendUrl = sendUrl.format(title,desc,url,source,image,category);	    			
+	e.preventDefault();
+	window.open(
+      sendUrl, 
+      'share-poshfeed', 
+      'width=626,height=436'
+    );  			    			
+};
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//      GET & BUILD TOP SITES BAR      /////////////////////////////////////////////////////////////
+function getTopSites(data){
+	chrome.topSites.get(buildTopSites);
+};
+function buildTopSites(data){
+	for (var i=0;data.length>i;i++) {
+		var thumbnailUrl = 'chrome://favicon/' + data[i].url;
+		$('#mostVisited ul').append("<li><a style='background-image:url("+thumbnailUrl+")' href='"+data[i].url+"'>"+data[i].title+"</a></li>");
+	}
 }
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//      LOAD USER CATEGORIES & GET THE CONTENT FROM SERVER      ////////////////////////////////////
+
+(function getCategories(){
+	chrome.storage.sync.get("categories", function(data){	
+		data = $(data.categories).toArray();
+		theCategories = data;
+		getContent(theCategories);
+	});
+})();
+
 
 function getContent(cats){
-	var xhr = new XMLHttpRequest();
-			xhr.open("GET", "http://poshfeed.com/getCategoryKey?categories="+cats, true);
-			xhr.onreadystatechange = function() {			
-	  		if (xhr.readyState == 4) {  
-	  			window.rawPFresponse = xhr.responseText;			 		
-	    		var responseObj = JSON.parse(xhr.responseText);    		
-	    		window.returnedJson = responseObj;
-	    		$('#contentReport').attr('href','http://poshfeed.com/?contactUs=4&urlToReport='+encodeURIComponent(responseObj.url));
-	    		$("#megaWrapper").css("background-image","url("+responseObj.image+")");
-	    		$("#pageTitle").html(responseObj.title).attr("href",responseObj.url).css("display","block").click(function(e){
-	    			e.preventDefault();
-	    			fireClickEvent(responseObj.url);    			    			
-	    		});	
-				if (responseObj.desc) {$("#pageDesc").html(responseObj.desc).attr("href",responseObj.url).css("display","block").click(function(e){
-	    			e.preventDefault();
-	    			fireClickEvent(responseObj.url);    			    			
-	    		});	
-	    		} //show only if description available
-	    		$("#sharePage").css("display","inline-block").click(function(e){
-	    			value = responseObj;
-	    			var sendUrl = 'http://poshfeed.com/shareURL?value="title":"{0}","desc":"{1}","url":"{2}","source":"{3}","image":"{4}","category":"{5}"';
-				    var title = encodeURIComponent(value.title.replace(/\"/g,'%22'));
-				    var desc = encodeURIComponent(value.desc.replace(/\"/g,'\%22'));
-				    var url = encodeURIComponent(value.url.replace(/\"/g,'%22'));
-				    var source = encodeURIComponent(value.source.replace(/\"/g,'%22'));
-				    var image = encodeURIComponent(value.image.replace(/\"/g,'%22'));
-				    var category = encodeURIComponent(value.category.replace(/\"/g,'%22'))
-				
-				    sendUrl = sendUrl.format(title,desc,url,source,image,category);	    			
-	    			e.preventDefault();
-	    			window.open(
-				      sendUrl, 
-				      'share-poshfeed', 
-				      'width=626,height=436'
-				    );  			    			
-	    		});
-				$("#pageSource").html(responseObj.source).css("display","");
-				$(".menuLink, .nextLink, .articleSource, #pageSource").css("display","");
-	  		}
-		}
-	xhr.send();
-}
-chrome.storage.sync.get("categories", function(data){	
-	data = $(data.categories).toArray();
-	var displayNameArray = [];
-	for(var i=0;i < data.length ; i++){
-        var catName = data[i];
-		displayNameArray.push(categoriesObj[catName].display)
-	}
-	$('#currentCats').text(displayNameArray.join(', '));
-	if(data.length == 0){
-		var allCats = [];
-		for(cat in categoriesObj){
-			allCats.push(cat);			
-		}
-		data = allCats;
-		chrome.storage.sync.set({'categories': allCats});
-	}
-	data = data.toString();	
+	content = $.ajax({
+		dataType: "json",
+		url: "http://poshfeed.com/getCategoryKey?categories="+cats,
+		success: buildPage
+	});
 	
-	$('#optionsLink').attr('href',chrome.extension.getURL("options.html"));
-	getContent(data);
-});
-
-
-
-
-function populateCategories(){
-	for (obj in categoriesObj) {
-		var value = categoriesObj[obj];
-		$('#pfCategories').append("<li><input type='checkbox' name='category' id='"+value.id+"' value='"+value.id+"'  /> <label for='"+value.id+"'><span></span>"+value.display+"</label></li>");
-		
-	}
-	//$(categoriesObj).each(function(){
-	//	//console.log($(this)[0].display);
-	//	$('#poshOptionsContainer').append("<li><input type='checkbox' name='category' id='"+$(this)[0].id+"' value='"+$(this)[0].id+"'  /> <label for='"+$(this)[0].id+"'>"+$(this)[0].display+"</label></li>");
-	//})
 }
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
+//      BUILD PAGE      ///////////////////////////////////////////////////////////////////////////
+function buildPage(content){
+	theContent = content;
+	$("#megaWrapper").css({'background-image':'url('+content.image+')'});
+	$("#pageTitle").text(content.title).show();
+	$("#pageDesc").text(content.desc).show();
+	$("#pageSource").text(content.source).show();
+	$('.mainLink').attr('href',content.url).on('click',fireClickEvent);
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+//      SAVE & RESTORE OPTIONS FROM STORAGE     ////////////////////////////////////////////////////
 function save_options() {
 	var checkedCats = $('input:checkbox:checked:enabled');
 	var categories = [];
 	$(checkedCats).each(function(){		
 		categories.push($(this).val());		
 	});
-	
 	chrome.storage.sync.set({'categories': categories});
-
-	$('#status').fadeIn();
-	setTimeout(function() {
-		$('#status').fadeOut();
-	}, 2000);
 	
-	
+	if(checkedCats.length == 0){
+		var allCats = [];
+		for(cat in categoriesObj){
+			console.log(cat);
+			allCats.push(cat);			
+		}
+		console.log(allCats);
+		chrome.storage.sync.set({'categories': allCats});
+	}
 }
 
-// Restores select box state to saved value from storage.
 function restore_options() {
-	savedCats = chrome.storage.sync.get("categories", function(data){		
+	chrome.storage.sync.get("categories", function(data){		
 		data = $(data.categories).toArray();
 		if(data.length > 0){		
 			$(data).each(function(){
@@ -144,9 +113,9 @@ function restore_options() {
 		}
 	})
 }
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-//open/close menu and attach events
+//      OPEN/CLOSE SIDEBAR AND ATTACH EVENTS     ///////////////////////////////////////////////////
 function toggleMenu(e) {
 	if ($('body').hasClass('open')) {
 		$('megaWrapper').unbind('click');
@@ -161,32 +130,63 @@ function toggleMenu(e) {
 		e.stopPropagation();
 	}
 }
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//      POPULATE SIDEBAR CATEGORIES     ////////////////////////////////////////////////////////////
+function populateCategories(){
+	for (obj in categoriesObj) {
+		var value = categoriesObj[obj];
+		$('#pfCategories').append("<li><input type='checkbox' name='category' id='"+value.id+"' value='"+value.id+"'  /> <label for='"+value.id+"'><span></span>"+value.display+"</label></li>");
+		
+	}
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//      GLOBAL RELOAD     //////////////////////////////////////////////////////////////////////////
+function reloadPage(){
+	chrome.tabs.reload();
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
-//reload on mousewheel
-function reloadPage(){chrome.tabs.reload()}
+function fireClickEvent(e){
+	_trackEvent('click', 'itemClick', 'click on main link',e.target);
+	e.preventDefault();
+	var clickEvent = $.ajax({
+		dataType: "json",
+		url: "http://poshfeed.com/addClickStat?value="+JSON.stringify(theContent)
+	});
+		
+	setTimeout(function(){window.location.href = theContent.url},200);
+}
+
 
 
 //document ready
 $(document).ready(function(){
+   
+    getTopSites();
+    populateCategories();
+    restore_options();
+    
+    
+    $('#nextLink').click(reloadPage);
+    $('#sharePage').click(getShareLink);
+    $('#menuLink').click(toggleMenu);
+    $('#pfCategories li input').change(save_options);
+    
     $('body').bind('mousewheel', function(e){
 	    e.stopPropagation();
         $('body').unbind('mousewheel');
         setTimeout(reloadPage, 300)
     });
-    
-    
-    $('#nextLink').click(reloadPage);
-    
-    $('#menuLink').click(toggleMenu);
 
-
-	
-	populateCategories();
-    restore_options();
-    $('#pfCategories li input').change(save_options);
 });
+
+
+
+
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-43001046-1']);
 _gaq.push(['_trackPageview']);
