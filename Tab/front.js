0a939718ce7f2ca4e54bc63751b421aeff53c3b9
Ohad Aviv <ohad@wibiya.com>
2013-10-17 14:44:35 +0300

    latest deploy 1.04

--- Tab/front.js
+++ Tab/front.js
@@ -213,6 +213,9 @@ $(document).ready(function(){
     $('#sharePage').click(getShareLink);
     $('#menuLink').click(toggleMenu);
     $('#pfCategories li input').change(save_options);
+    
+    
+    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 
 });
 
