7dbf4fd5cb84000dbe42b7bc5faca497572f329d
Ohad Aviv <ohad@wibiya.com>
2014-04-07 14:43:45 +0300

    changed pocket timing

--- Tab/front.js
+++ Tab/front.js
@@ -232,7 +232,7 @@ function startTime(){
 //pocket script execution
 function loadPocket(){
 	!function(d,i){if(!d.getElementById(i)){var j=d.createElement("script");j.id=i;j.src="https://widgets.getpocket.com/v1/j/btn.js?v=1";var w=d.getElementById(i);d.body.appendChild(j);}}(document,"pocket-btn-js");
-	$('#pocket-btn-js').on('load',function(e){console.log('loaded');$('.pocket-btn').delay(500).animate({opacity: 1}, 1500)});	
+	$('#pocket-btn-js').on('load',function(e){console.log('loaded');$('.pocket-btn').delay(500).animate({opacity: 1}, 500)});	
 }
 
 //document ready
