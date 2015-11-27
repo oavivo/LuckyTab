e39d62bd7ba7400c0291983f6b42da6d09402cac
Ohad Aviv <ohad@muz.li>
2015-11-28 00:37:53 +0200

    added analytics

--- Tab/front.js
+++ Tab/front.js
@@ -194,7 +194,22 @@ $(document).ready(function(){
     $('#nextLink').click(reloadPage);
     //$('#sharePage').click(getShareLink);
     $('#menuLink').click(toggleMenu);
-    
+    $("#pageTitle, #pageDesc").on("click", function(){
+      ga('send', 'event', "post", "click", $("#pageTitle").text());
+    })
     
     
 });
+
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-43001046-1', 'auto');
+  ga('set', 'checkProtocolTask', function(){}); // Removes failing protocol check. @see: http://stackoverflow.com/a/22152353/1958200
+  ga('require', 'displayfeatures');
+  ga('send', 'pageview', '/front.html');
+
+
